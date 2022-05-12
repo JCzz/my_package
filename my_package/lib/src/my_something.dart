07f5197e782b9a9243e2ee45458976d6d082d4a8
JCzz <awear@appsyouwear.com>
2022-05-12 18:47:58 +0200

    platform

--- my_package/lib/src/my_something.dart
+++ my_package/lib/src/my_something.dart
@@ -5,3 +5,7 @@ import 'package:my_package_platform_interface/my_package_platform_interface.dart
 Future<bool> canLaunchUrl(Uri url) async {
   return await MyPackagePlatform.instance.canLaunch(url.toString());
 }
+
+Future<String?> getPlatform() async {
+  return await MyPackagePlatform.instance.platformVersion;
+}
